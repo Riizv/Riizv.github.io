8447098dba6645774fc657335719c434919d6533
kcpru <kacperuczyk@gmail.com>
2020-07-30 13:23:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0379c8e33463a26d74a356aa92fa899d.js"
+  "/precache-manifest.dce08295d06e188e858fd1f07158287f.js"
 );
 
 self.addEventListener('message', (event) => {
